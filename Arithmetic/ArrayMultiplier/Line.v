45063d46ef7282f49f4e5114fd6642d36b87f723
Mohamed Ammar <62853363+Mohamed-Ammar@users.noreply.github.com>
2021-01-30 04:18:17 +0200

    changed 8 bits z to 4 bits

--- Arithmetic/ArrayMultiplier/Line.v
+++ Arithmetic/ArrayMultiplier/Line.v
@@ -20,17 +20,13 @@
 //////////////////////////////////////////////////////////////////////////////////
 module Line( 
 	input  x,
-	input  [7:0] y,
-	output [7:0] z
+	input  [3:0] y,
+	output [3:0] z
     );
 	 
  assign z[0] = x & y[0];
  assign z[1] = x & y[1];
  assign z[2] = x & y[2];
  assign z[3] = x & y[3]; 
- assign z[4] = x & y[4];
- assign z[5] = x & y[5];
- assign z[6] = x & y[6];
- assign z[7] = x & y[7];
 
 endmodule
